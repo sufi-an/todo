57556bc47ea724269a498208697500f7e1ce5b23
saurisufian <abusauri.iub@gmail.com>
2021-05-01 13:56:28 +0600

    v-1.0.1

--- src/Components/CompletedTasks/CompletedTasks.js
+++ src/Components/CompletedTasks/CompletedTasks.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import CompletedTask from './CompletedTask/CompletedTask';
+import classes from './CompletedTask.module.css'
 const completedTasks=(props)=>{
-    //console.log(props.list)
+    let history=props.list.map((element)=>{
+        return <CompletedTask key={element.id}  data={element}/>
+    });
+   if(history.length === 0){
+      history=<h1>No History Found</h1>
+   }
+    
     return(
-        <div>
-        {
-            props.list.map((element)=>{
-                return <CompletedTask key={element.id}  data={element}/>
-            })
-            }
+        <div className={classes.CompletedTask}>
+        {history}
         </div>
     );
 }
--- src/Components/Tasks/Task/Task.js
+++ src/Components/Tasks/Task/Task.js
@@ -5,6 +5,7 @@ class Task extends Component{
   state={
     time: ""
   }
+
   has31Days=(month)=>{
     let listOf31DayMonths=['1','3','5','7','8','10','12'];
     for(let i=0;i<listOf31DayMonths.length;i++){
@@ -35,23 +36,43 @@ class Task extends Component{
       days=(finish[2]-(parseInt(start[2])+28))
       month-=1;
     }
+    let result=null;
+    if(year.toString() === '0' && month.toString() ==='0' && days.toString() === '0'){
+      return '';
+    }
+    else if(year.toString() === '0' && month.toString() ==='0'){
+     // console.log(year+":"+month+":"+days)
+      return (parseInt(days)-1).toString()+" Days";
+    }
+    else if(year.toString() === '0'){
+      return month+" Months "+ (parseInt(days)-1).toString()+" Days";
+    }
+
     //console.log(year+" "+month+" "+ (parseInt(days)-1).toString())
-    return year+" "+month+" "+ (parseInt(days)-1).toString();
+    else{ 
+      //console.log(":"+year+":"+month+":"+days)
+    return year+" Years "+month+" Months "+ (parseInt(days)-1).toString()+" Days";
+    }
   }
   getData(data){
     let time=new Date();
+    let seconds=time.getSeconds()<10?"0"+59-time.getSeconds():59-time.getSeconds();
+    let minutes=time.getMinutes()<10?"0"+59-time.getMinutes():59-time.getMinutes();
+    let hours=time.getHours()<10?"0"+23-time.getHours():25-time.getHours();
     time=(time.getHours()<10?"0"+time.getHours():time.getHours())
       +":"+(time.getMinutes()<10?"0"+time.getMinutes():time.getMinutes())
-      +":"+(time.getSeconds()<10?"0"+time.getSeconds():time.getSeconds());
+      +":"+(time.getSeconds()<10?"0"+59-time.getSeconds():59-time.getSeconds());
     //console.log(time)
+    //this.props.data.issueTime='0:0';
     let startingTime=this.props.data.issueTime.split(":");
     let startingHour=startingTime[0];
     
     let startingMinute=startingTime[1];
-    
+    let getRemainingHour=hours;
+    let getRemainingMinutes=minutes;
     let daysLeft=this.getRemainingDays(this.props.data.deadline,this.props.data.issueDate);
-    console.log(time)
-    return time;
+    //console.log(getRemainingHour+" "+getRemainingMinutes+" "+seconds)
+    return this.getRemainingDays(this.props.data.deadline,this.props.data.issueDate)+" "+ getRemainingHour+" Hours : "+getRemainingMinutes+" Minutes :  "+seconds+" Seconds";
   }
   componentDidMount() {
     
@@ -90,11 +111,9 @@ class Task extends Component{
         <div>
         <span>Due Date : </span> {this.props.data.deadline}
         </div>
+        <div>Remaining Time </div>
         <div>
-        <span>Issue Time : </span>{this.props.data.issueTime}
-        </div>
-        <div>
-          {this.state.time}
+          <span style={{fontSize:".8rem"}}> {this.state.time} </span>
         </div>
         <div className={classes.BtnSuccess}>
         <Button clicked={()=>this.props.onClicked(this.props.data.id)} >Task Done</Button>
